ab0190064bb6edd0559ce6bae57796462c93a598
pradoppc <prado.ppc@gmail.com>
2022-04-04 20:02:47 -0300

    msg

--- app/views/master.php
+++ app/views/master.php
@@ -8,6 +8,13 @@
     <link rel="stylesheet" type="text/css" href="/assets/css/styles.css">
 </head>
 <body>
-    <div class="header"> <?php require 'partials/header.php'; ?> </div>
+    <div id="header"> 
+        <?php require 'partials/header.php'; ?> 
+    </div>
+
+    <div class="container">
+        <?php require VIEWS.$view; ?>
+    </div>
+
 </body>
 </html>
