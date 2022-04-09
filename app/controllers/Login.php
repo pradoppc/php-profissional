0af41408158c27f7602a260e96826b7dc5d375bc
pradoppc <prado.ppc@gmail.com>
2022-04-09 11:44:35 -0300

    alter

--- app/controllers/Login.php
+++ app/controllers/Login.php
@@ -11,6 +11,25 @@
 
         public function store(){
 
+            $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+            $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_EMAIL);
+
+            if (empty($email) || empty($password)){
+                return redirect('/login');
+            }
+
+            $user = findBy('users', 'email', $email);
+
+            if (!$user){
+                return redirect('/login');
+            }
+
+            if (!password_verify($password, $user->password)){
+                return redirect('/login');
+            }
+
+            $_SESSION['logged'] = $user;
+            return redirect('/');
         }
     }
 ?>
--- app/views/login.php
+++ app/views/login.php
@@ -2,7 +2,7 @@
 
 
 <form action="/login" method="post" id="box-login">
-    <input type="text" name="email" placeholder="Seu e-mail">
-    <input type="password" name="password" placeholder="Sua senha">
+    <input type="text" name="email" placeholder="Seu e-mail" value="prado.ppc@gmail.com">
+    <input type="password" name="password" placeholder="Sua senha" value="123">
     <button type="submit"> Login </button>
 </form>
